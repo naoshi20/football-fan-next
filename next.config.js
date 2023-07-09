847f9a582680f2e006d2ce3042f0e4bcaf98340b
naoshi20 <naoshi.st.paul@gmail.com>
2023-07-09 22:24:35 +0900

    cssからscssへ移行

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  // variableは各sassファイルがコンパイルされる前に各sassファイルに追加されておく必要がある
+  // variableは各scssファイルがコンパイルされる前に各sassファイルに追加されておく必要がある
+  // prependDataの値が各scssファイルの先頭に追加されるイメージ
   sassOptions: {
     includePaths: [path.join(__dirname, 'src', 'styles')],
     prependData: `@import "/styles/_variables.scss";`
