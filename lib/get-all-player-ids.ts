b433a5b4b484b2d0c5803ccf9f66c2f2db4d00b7
naoshi20 <naoshi.st.paul@gmail.com>
2023-07-15 01:59:17 +0900

    ローカル開発では取得データ数を制限するよう修正

--- lib/get-all-player-ids.ts
+++ lib/get-all-player-ids.ts
@@ -4,21 +4,29 @@ export async function getAllPlayerIds(): Promise<
   { [key: string]: { id: any } }[] | null
 > {
   const supabase = createClientComponentClient()
+  const env = process.env.NODE_ENV
 
   try {
-    const { data, error, status } = await supabase
-      .from('players5')
-      .select('*')
-      .order('id', { ascending: true })
-      .limit(10)
+    let result
+    if (env == 'development') {
+      result = await supabase
+        .from('players5')
+        .select('*')
+        .order('id', { ascending: true })
+        .limit(10)
+    }
 
-    if (error && status !== 406) {
-      throw error
+    if (env == 'production') {
+      result = await supabase
+        .from('players5')
+        .select('*')
+        .order('id', { ascending: true })
     }
-    if (!data) {
-      throw error
+
+    if (result.error && result.status !== 406) {
+      throw result.error
     }
-    return data.map(player => {
+    return result.data.map(player => {
       console.log(player)
       console.log(player.id.toString())
       return {
--- lib/get-players.ts
+++ lib/get-players.ts
@@ -2,19 +2,30 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 export async function getPlayers(): Promise<any> {
   const supabase = createClientComponentClient()
+  const env = process.env.NODE_ENV
 
   try {
-    const { data, error, status } = await supabase
-      .from('players5')
-      .select('*')
-      .order('id', { ascending: true })
-      .limit(10)
-
-    if (error && status !== 406) {
-      throw error
+    let result
+    if (env == 'development') {
+      result = await supabase
+        .from('players5')
+        .select('*')
+        .order('id', { ascending: true })
+        .limit(10)
     }
 
-    return data
+    if (env == 'production') {
+      result = await supabase
+        .from('players5')
+        .select('*')
+        .order('id', { ascending: true })
+    }
+
+    if (result.error && result.status !== 406) {
+      throw result.error
+    }
+
+    return result.data
   } catch (error) {
     console.log('Error loading user data!')
   }
