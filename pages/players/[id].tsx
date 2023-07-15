d7bed088438f753db49e93c770017f5bf7c0bd2c
naoshi20 <naoshi.st.paul@gmail.com>
2023-07-16 01:20:40 +0900

    個別ページのレイアウトを整える

--- pages/players/[id].tsx
+++ pages/players/[id].tsx
@@ -42,32 +42,91 @@ export default function Player({ playerData }) {
         <title>{siteTitle}</title>
       </Head>
       <article>
-        <h1 className="">{playerData.japanese_name}</h1>
-        <p className="">{playerData.belongings}</p>
-        {flagUrl ? (
-          <Image
-            width={22}
-            height={14}
-            src={flagUrl}
-            alt="Avatar"
-            className="avatar image"
-            style={{ height: 14, width: 22 }}
-          />
-        ) : (
-          <></>
-        )}
-        {teamImageUrl ? (
-          <Image
-            width={22}
-            height={22}
-            src={teamImageUrl}
-            alt="Avatar"
-            className="avatar image"
-            style={{ height: 22, width: 22 }}
-          />
-        ) : (
-          <></>
-        )}
+        <div className="w-full flex justify-center">
+          <div>
+            {playerData.img ? (
+              <Image
+                width={220}
+                height={220}
+                src={playerData.img}
+                alt="Avatar"
+                className="avatar image"
+                style={{ height: 220, width: 220 }}
+              />
+            ) : (
+              <></>
+            )}
+            <div className="flex justify-center mt-6">
+              <h1 className="">{playerData.japanese_name}</h1>
+            </div>
+            <div className="flex justify-center mt-3">
+              <div className="flex">
+                {flagUrl ? (
+                  <Image
+                    width={22}
+                    height={14}
+                    src={flagUrl}
+                    alt="Avatar"
+                    className="avatar image mr-3"
+                    style={{ height: 14, width: 22 }}
+                  />
+                ) : (
+                  <></>
+                )}
+
+                {teamImageUrl ? (
+                  <Image
+                    width={22}
+                    height={22}
+                    src={teamImageUrl}
+                    alt="Avatar"
+                    className="avatar image"
+                    style={{ height: 22, width: 22 }}
+                  />
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+            <div className="mt-6">
+              <ul>
+                <li
+                  className="w-full flex justify-between py-2"
+                  style={{ textAlign: 'center' }}
+                >
+                  <p className="w-1/2">ポジション</p>
+                  <p className="w-1/2">{playerData.position}</p>
+                </li>
+                <hr
+                  style={{
+                    color: '#aaaaaa',
+                    height: '4px'
+                  }}
+                />
+                <li
+                  className="w-full flex justify-between  py-2"
+                  style={{ textAlign: 'center' }}
+                >
+                  <p className="w-1/2">背番号</p>
+                  <p className="w-1/2">{playerData.back_number}</p>
+                </li>
+                <hr
+                  style={{
+                    color: '#aaaaaa',
+                    height: '4px'
+                  }}
+                />
+                <li
+                  className="w-full flex justify-between py-2"
+                  style={{ textAlign: 'center' }}
+                >
+                  <p className="w-1/2">誕生日</p>
+                  <p className="w-1/2">{playerData.birthday}</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </article>
     </Base>
   )
