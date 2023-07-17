8581e77de8903ae5f1f963c5b50c951a2c194c63
naoshi20 <naoshi.st.paul@gmail.com>
2023-07-17 19:55:32 +0900

    スターボタンを押下しても親コンポーネントのLinkコンポーネントが動作しないよう修正

--- components/star-button/star-button.component.tsx
+++ components/star-button/star-button.component.tsx
@@ -2,9 +2,17 @@ import card from '../card/card.module.scss'
 import starButton from './star-button.module.scss'
 
 export default function StarButton({ playerId, favorite, clickCallBack }) {
+  const handleClick = (playerId: number) => {
+    clickCallBack(playerId)
+  }
   return (
-    <div className={card.starButton}>
-      <button onClick={() => clickCallBack(playerId)}>
+    <div className={`${card.starButton} p-2`}>
+      <button
+        onClick={e => {
+          e.preventDefault() // prevent page transition by link component
+          handleClick(playerId)
+        }}
+      >
         <i
           className={`fa-star
           ${
