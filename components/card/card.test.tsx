bd9bece324e554f531800b62a23b35d3e628c2ff
naoshi20 <naoshi.st.paul@gmail.com>
2023-07-26 16:25:59 +0900

    カードコンポーネントの単体テストを修正

--- components/card/card.test.tsx
+++ components/card/card.test.tsx
@@ -1,76 +1,82 @@
-import userEvent from '@testing-library/user-event'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Card from './card.component'
 
-const user = userEvent.setup()
-test('カードをレンダリングすると、リンクの遷移先が正しい遷移先になっていること', async () => {
-  const mockFn = jest.fn()
-  render(
-    <Card
-      id={1}
-      img={
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p225321.png'
-      }
-      japanese_name={'テスト'}
-      belongings={'arsenal'}
-      favorite={true}
-      clickCallBack={mockFn}
-    />
-  )
-  const aTag = screen.getByRole('link')
-  expect(aTag).toHaveAttribute('href', `/players/1`)
+// StarButtonコンポーネントのモック作成
+jest.mock('../star-button/star-button.component', () => {
+  return function DummyStarButton({ favorite }) {
+    return <div data-testid="star-button">{favorite}</div>
+  }
 })
 
-test('カードをレンダリングすると、スターボタンがレンダリングされていること', async () => {
-  const mockFn = jest.fn()
-  render(
-    <Card
-      id={1}
-      img={
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p225321.png'
-      }
-      japanese_name={'テスト'}
-      belongings={'arsenal'}
-      favorite={true}
-      clickCallBack={mockFn}
-    />
-  )
-  await user.click(screen.getByRole('button'))
-  expect(mockFn).toBeCalled()
-})
+describe('Card Componentをレンダリングした時、', () => {
+  let getByRole, getByTestId, clickCallBack, aTag, starButton, img, imgSrc
 
-test('imgが存在する場合には、該当の画像が表示されること', async () => {
-  const mockFn = jest.fn()
-  render(
-    <Card
-      id={1}
-      img={
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p225321.png'
+  beforeEach(() => {
+    clickCallBack = jest.fn()
+  })
+  describe('選手データが存在する場合、', () => {
+    beforeEach(() => {
+      imgSrc = 'https://example.com/player.jpg'
+      const props = {
+        id: '1',
+        japanese_name: 'テスト',
+        belongings: 'テスト',
+        favorite: false,
+        img: imgSrc,
+        clickCallBack: clickCallBack
       }
-      japanese_name={'テスト'}
-      belongings={'arsenal'}
-      favorite={true}
-      clickCallBack={mockFn}
-    />
-  )
-  await user.click(screen.getByRole('button'))
-  expect(mockFn).toBeCalled()
-})
 
-test('imgが存在しない場合には、サンプル画像が表示されること', async () => {
-  const mockFn = jest.fn()
-  render(
-    <Card
-      id={1}
-      img={
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p225321.png'
+      const renderResult = render(<Card {...props} />)
+
+      getByRole = renderResult.getByRole
+      getByTestId = renderResult.getByTestId
+      aTag = getByRole('link')
+      img = getByRole('img')
+      starButton = getByTestId('star-button')
+    })
+
+    test('リンクの遷移先が正しい遷移先になっていること', async () => {
+      expect(aTag).toHaveAttribute('href', `/players/1`)
+    })
+
+    test('スターボタンがレンダリングされていること', async () => {
+      expect(starButton).toBeInTheDocument()
+    })
+
+    test('imgが存在する場合には、該当の画像が表示されること', async () => {
+      expect(img).toBeInTheDocument()
+      expect(img).toHaveAttribute(
+        'src',
+        expect.stringContaining(encodeURIComponent(imgSrc))
+      )
+    })
+  })
+
+  describe('選手データが存在する場合、', () => {
+    let defaultImgSrc
+
+    beforeEach(() => {
+      defaultImgSrc = '/images/sample-person.jpg'
+      const props = {
+        id: '123',
+        japanese_name: 'テスト',
+        belongings: 'テスト',
+        favorite: false,
+        img: '',
+        clickCallBack: clickCallBack
       }
-      japanese_name={'テスト'}
-      belongings={'arsenal'}
-      favorite={true}
-      clickCallBack={mockFn}
-    />
-  )
-  await user.click(screen.getByRole('button'))
-  expect(mockFn).toBeCalled()
+      const renderResult = render(<Card {...props} />)
+
+      getByRole = renderResult.getByRole
+      img = getByRole('img')
+    })
+
+    test('サンプル画像が表示されること', async () => {
+      expect(img).toBeInTheDocument()
+      expect(img).toHaveAttribute(
+        'src',
+        expect.stringContaining(encodeURIComponent(defaultImgSrc))
+      )
+    })
+  })
 })
